119db9f28fd30e5be7207434bd714c4eb49303a5
Sampada <sampada>
2023-06-30 20:47:18 +0530

    Initial Commit

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ from functools import wraps
 
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = 'sampu'
 ckeditor = CKEditor(app)
 Bootstrap(app)
 login_manager = LoginManager()
